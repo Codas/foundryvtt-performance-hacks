8a4ec29f7042aa9f991975b4779e4a2aea2598ed
Arne Link <mail@arne-link.de>
2025-02-25 12:56:06 +0100

    Cache token icons individually if effect hider is active
    resolves #16

--- src/hacks/effects-caching.ts
+++ src/hacks/effects-caching.ts
@@ -1,6 +1,20 @@
 import { SETTINGS, getSetting } from 'src/settings.ts';
 import { getBitmapCacheResolution } from 'src/utils/getBitmapCacheResolution.ts';
 
+function refreshEffectCache(object: PIXI.DisplayObject) {
+	object.cacheAsBitmap = false;
+	object.cacheAsBitmapResolution = getBitmapCacheResolution();
+	object.cacheAsBitmap = true;
+}
+
+function shouldCacheIndividualEffects() {
+	const effectHider = game.modules.get('effect-hider');
+	if (effectHider && effectHider.active) {
+		return true;
+	}
+	return false;
+}
+
 async function cacheEffects(this: Token, wrapper: Function, ...args: any[]) {
 	const wrappedResult = wrapper(...args);
 	if (wrappedResult instanceof Promise) {
@@ -9,9 +23,13 @@ async function cacheEffects(this: Token, wrapper: Function, ...args: any[]) {
 
 	const [flags] = args;
 	if (flags?.redrawEffects || flags?.refreshEffects) {
-		this.effects.cacheAsBitmap = false;
-		this.effects.cacheAsBitmapResolution = getBitmapCacheResolution();
-		this.effects.cacheAsBitmap = true;
+		if (shouldCacheIndividualEffects()) {
+			this.effects.children.forEach((effect) => {
+				refreshEffectCache(effect);
+			});
+		} else {
+			refreshEffectCache(this.effects);
+		}
 	}
 }
 
